d584c4de29660f2331eab151656c465864a425c6
Unknown <jarielle.lim@gmail.com>
2017-04-09 09:02:09 -0700

    added noted, and citation sheet

--- js/main.js
+++ js/main.js
@@ -5,6 +5,9 @@ var $grid = $('.grid').isotope({
   layoutMode: 'masonry'
 });
 
+$grid.imagesLoaded().progress( function() {
+  $grid.isotope('layout');
+});
 
 // filter items on button click
 $('.filter-button-group').on( 'click', 'button', function() {
@@ -20,3 +23,32 @@ $('.button-group > button').on('click', function() {
 // initialize parallax~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 $('.no-parallax').addClass('parallax-window')
 $('.parallax-window').parallax({imageSrc: 'img/gradient.png'});
+
+//add image masonry
+function getRandomSize(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+var allImages = "";
+
+for (var i = 0; i < 25; i++) {
+  var width = getRandomSize(200, 400);
+  var height =  getRandomSize(200, 400);
+
+}
+
+$('#photos').append(allImages);
+
+// smoothscrolling~~~~~~~~~~~~~
+$('a[href*="#"]:not([href="#"])').click(function() {
+  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    if (target.length) {
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000);
+      return false;
+    }
+  }
+});
